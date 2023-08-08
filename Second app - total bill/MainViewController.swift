2c98d64ce0a4b18fe84bda48228bd81c938b3f2a
ncheorghiev <nicolai.gheorghiev@iis.utm.md>
2023-08-08 20:28:13 +0300

    First version

---
+++ Second app - total bill/MainViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  Second app - total bill
+//
+//  Created by Home on 08.08.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Total Bill"
+        label.textColor = .black
+        label.font = UIFont(name: "Avenir Next Bold", size: 40)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Enter the invoice amount and click \"Calculate\""
+        label.textColor = .black
+        label.font = UIFont(name: "Avenir Next Bold", size: 15)
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.7
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var calculateButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Calculate", for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = #colorLiteral(red: 0.6390794516, green: 0.2492685616, blue: 0.6254444718, alpha: 1)
+        button.titleLabel?.font = UIFont(name: "Avenir Next", size: 20)
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    let totalBillView = TotalBillView()
+    let personsView = PersonsView()
+    
+    override func viewDidLoad() { // Метод, с которого начинается загрузка
+        super.viewDidLoad()
+        
+        setupViews()
+        setConstraints()
+    }
+    
+    func setupViews() { // настройка объектов
+        view.backgroundColor = #colorLiteral(red: 0.9813231826, green: 0.9813460708, blue: 0.9813337922, alpha: 1)
+        
+        view.addSubview(titleLabel)
+        view.addSubview(logoImageView)
+        view.addSubview(descriptionLabel)
+        view.addSubview(totalBillView)
+        view.addSubview(personsView)
+        view.addSubview(calculateButton)
+    }
+}
+
+extension MainViewController {
+    
+    func setConstraints() { // Расположение объектов
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            logoImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            logoImageView.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 15),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            totalBillView.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 10),
+            totalBillView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            totalBillView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            totalBillView.heightAnchor.constraint(equalToConstant: 130),
+            
+            personsView.topAnchor.constraint(equalTo: totalBillView.bottomAnchor, constant: 10),
+            personsView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            personsView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            personsView.heightAnchor.constraint(equalToConstant: 130),
+            
+            calculateButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            calculateButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            calculateButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            calculateButton.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+}
---
+++ Second app - total bill/TotalBillView.swift
@@ -0,0 +1,64 @@
+//
+//  TotalBillView.swift
+//  Second app - total bill
+//
+//  Created by Home on 08.08.2023.
+//
+
+import UIKit
+
+class TotalBillView: UIView {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Total Bill"
+        label.textColor = #colorLiteral(red: 0.2454476058, green: 0.2894899547, blue: 0.3496131301, alpha: 1)
+        label.font = UIFont(name: "Avenir Next", size: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let summTextField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = #colorLiteral(red: 0.9300000072, green: 0.9300000072, blue: 0.9300000072, alpha: 1)
+        textField.layer.cornerRadius = 10
+        textField.textColor = .black
+        textField.font = UIFont(name: "Avenir Next", size: 48)
+        textField.textAlignment = .center
+        textField.keyboardType = .numberPad
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupView()
+        setContraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(titleLabel)
+        addSubview(summTextField)
+    }
+    
+    func setContraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 0),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            
+            summTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 2),
+            summTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
+            summTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0),
+            summTextField.heightAnchor.constraint(equalToConstant: 100),
+            
+        
+        ])
+    }
+}
