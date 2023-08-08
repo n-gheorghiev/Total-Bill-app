6244ee5d6e2320eb56d0134b3bec1dbdf1b236b1
ncheorghiev <nicolai.gheorghiev@iis.utm.md>
2023-08-08 17:56:00 +0300

    Initial Commit

---
+++ Second app - total bill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Second app - total bill
+//
+//  Created by Home on 08.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
